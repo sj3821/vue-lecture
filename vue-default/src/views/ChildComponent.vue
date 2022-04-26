901a93db973c2f778735b01ffb15819004b03444
sj3821 <sj3821@gmail.com>
2022-04-26 16:53:47 +0900

    20220426 1653

---
+++ vue-default/src/views/ChildComponent.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+    <div>likes: {{likes}}</div>
+    <div>isOk:{{isOk}}</div>
+    <div>commentIds:{{commentIds}}</div>
+    <div>author name: {{author.name}}</div>
+    <div> author company:{{author.company}} </div>
+    <br><br><br>
+    <button type="button" @click="childFunc" ref="child_btn">자식에 있는 클릭</button>
+
+</div>
+</template>
+<script>
+
+export default{ 
+    name:'ChildComponent',
+    props:{
+        likes:{
+            type: Number,
+            default: 0
+        },
+        isOk:{
+            type: Boolean,
+            default: false
+        },
+        commentIds:{
+            type: Array
+        },
+        author:{
+            type:Object
+        }
+    },
+    components:{},
+    data(){
+        return{
+            sampleData:''
+        };
+    },
+    setup(){},
+    created(){},
+    mounted(){
+        console.log(this.commentIds);
+    },
+    unmounted(){},
+    methods:{
+        childFunc(){
+            alert('부모 컴포넌트에서 직접 발생시킨 이벤트');
+        }
+    }
+}
+</script>
--- vue-default/src/views/ComponentExam.vue
+++ vue-default/src/views/ComponentExam.vue
@@ -1,23 +1,40 @@
 <template>
 <div>
-    <PageTitle :title="title" />
+    <!-- <PageTitle :title="title" /> --> 
+
+   
+    
+    <page-title :title="title" />
+    <button type="button" @click="callChildFunc">부모에 있는 클릭</button>
+   <ChildComponent :likes="23" :isOk="isOk" :commentIds="commentIds" :author="author" ref="child_component" /> 
+    <!-- :likes="23" 처럼 바로 숫자로 선언하면 텍스트가 아닌 숫자로 인식함 -->
 </div>
 </template>
 <script>
 import PageTitle from '../components/PageTitle';
+import ChildComponent from './ChildComponent';
 
 export default{ 
     name:'ComponentExam',
-    components:{PageTitle},
+    components:{'page-title': PageTitle, ChildComponent},
     data(){
         return{
-            title:'부모컴포넌트에서 전송할 페이지 타이틀'
+            title:'부모컴포넌트에서 전송할 페이지 타이틀',
+            likes:23,
+            isOk:true,
+            commentIds:[1,5,2,3],
+            author:{name:'홍길동',company:'회사이름'}
         };
     },
     setup(){},
     created(){},
     mounted(){},
     unmounted(){},
-    methods:{}
+    methods:{
+        callChildFunc(){
+            // this.$refs.child_component.$refs.child_btn.click(); // 객체 접근 $refs 사용
+            this.$refs.child_component.childFunc(); //함수접근시에는 그냥 함수만 쓰면 됨
+        }
+    }
 }
 </script>
