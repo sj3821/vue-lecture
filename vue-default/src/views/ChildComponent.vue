0261734a085ea273ad2ac01edb702fbb558d1ed1
sj3821 <sj3821@gmail.com>
2022-04-26 18:00:36 +0900

    2022026 18:00

--- vue-default/src/views/ChildComponent.vue
+++ vue-default/src/views/ChildComponent.vue
@@ -7,7 +7,11 @@
     <div> author company:{{author.company}} </div>
     <br><br><br>
     <button type="button" @click="childFunc" ref="child_btn">자식에 있는 클릭</button>
-
+    <br><br><br>
+    <button type="button" @click="sendFromChild">자식 컴포넌트 버튼</button>
+    <br><br>
+    <button type="button" @click="changeData"> 자식 컴포넌트 데이터 변경</button>
+    <br><br>
     <h1>{{msg}}</h1>
 
 </div>
@@ -37,7 +41,8 @@ export default{
         return{
             sampleData:'',
             msg:'자식에 있던 메세지',
-            msg1:'자식 컴포넌트로부터 보내는 메세지'
+            msg1:'자식 컴포넌트로부터 보내는 메세지',
+            msg2:'자식 컴포넌트로부터 보내는 메세지'
         };
     },
     setup(){},
@@ -49,6 +54,12 @@ export default{
     methods:{
         childFunc(){
             alert('부모 컴포넌트에서 직접 발생시킨 이벤트');
+        },
+        sendFromChild(){
+            this.$emit('send-message', this.msg1); //커스텀이벤트 자식-> 부모 이벤트 
+        },
+        changeData(){
+            this.msg2='자식 컴포넌트에서 데이터 변경이 일어났습니다.';
         }
     }
 }
--- vue-default/src/views/ComponentExam.vue
+++ vue-default/src/views/ComponentExam.vue
@@ -6,8 +6,11 @@
     
     <page-title :title="title" />
     <button type="button" @click="callChildFunc">부모에 있는 클릭</button>
-   <ChildComponent :likes="23" :isOk="isOk" :commentIds="commentIds" :author="author" ref="child_component" /> 
+    <h1>{{parentMsg}}</h1>
+   <ChildComponent :likes="23" :isOk="isOk" :commentIds="commentIds" :author="author" ref="child_component" @send-message="sendMessage" /> 
     <!-- :likes="23" 처럼 바로 숫자로 선언하면 텍스트가 아닌 숫자로 인식함 -->
+    <button type="button" @click="showData">부모 버튼</button>
+    
 </div>
 </template>
 <script>
@@ -23,8 +26,14 @@ export default{
             likes:23,
             isOk:true,
             commentIds:[1,5,2,3],
-            author:{name:'홍길동',company:'회사이름'}
+            author:{name:'홍길동',company:'회사이름'},
+            parentMsg:''
         };
+    },
+    computed:{
+        msg2(){
+            return this.$refs.child_component.msg2;
+        }
     },
     setup(){},
     created(){},
@@ -35,6 +44,13 @@ export default{
             // this.$refs.child_component.$refs.child_btn.click(); // 객체 접근 $refs 사용
             // this.$refs.child_component.childFunc(); //함수접근시에는 그냥 함수만 쓰면 됨
             this.$refs.child_component.msg ='부모컴포넌트에서 변경한 메세지';
+        },
+        sendMessage(data){
+            alert(data);
+            this.parentMsg=data;
+        },
+        showData(){
+            alert(this.msg2);
         }
     }
 }
